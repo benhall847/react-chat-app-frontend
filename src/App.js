c604bb38893177b4642bfc7a0e5b4ec43b62146b
Benjamin Hallquist <benhall84707@gmail.com>
2019-05-03 13:18:31 -0400

    finshed frontend chatapp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ChatList from './ChatList'
+import axios from 'axios'
+import ChatForm from './ChatForm'
+import qs from 'qs'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+      this.state = {
+        messages: [],
+        newMessage:''
+      };
+    };
+
+componentDidMount(){
+    setInterval(async()=>{
+      let {data} = await axios.get('/api')
+      this.setState({
+        messages:data
+      })
+    }, 2000);
+  }
+  _postMessage = async()=>{
+    console.log(this.state.newMessage)
+    await axios({
+      method:'post',
+      url:'/api',
+      data: qs.stringify({
+
+        message:this.state.newMessage
+      }),
+      headers: {
+        'Content-Type':'application/x-www-form-urlencoded'
+      }
+    })
+    this.setState({
+      newMessage: ''
+    })
+  }
+  _changeHandler = (newMessage)=>{
+    this.setState({
+      newMessage
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+          <ChatList
+            messages={this.state.messages}
+          />
+
+          <ChatForm
+            clickHandler={this._postMessage}
+            newMessage={this.state.newMessage}
+            changeHandler={this._changeHandler}
+          
+          />
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
